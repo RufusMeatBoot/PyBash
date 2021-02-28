855df0dc8bc7a21514ce598b4ac3b5687de06a95
RufusMeatBoot <71909100+RufusMeatBoot@users.noreply.github.com>
2021-02-28 09:42:29 -0800

    Add files via upload

---
+++ PyBash/main.py
@@ -0,0 +1,116 @@
+from cmd import Cmd
+import os
+import socket
+import time
+import platform
+
+
+class Prompt(Cmd):
+
+    user = socket.gethostname()
+
+    def do_echo(self, args):
+        """Print a string."""
+
+        print(args)
+
+    def do_exit(self, args):
+        """Exit the shell."""
+
+        try:
+            raise SystemExit
+        except:
+            pass
+
+    def do_clear(self, args):
+        """Clear the shell."""
+
+        if platform == "linux" or platform == "linux2":
+            try:
+                os.system('clear')
+            except:
+                pass
+        elif platform == "win32" or platform == "win64":
+            try:
+                os.system('cls')
+            except:
+                pass
+
+    def do_cd(self, args):
+        """Change current working directory."""
+
+        try:
+            if len(args) > 0:
+                try:
+                    os.chdir(args)
+                except:
+                    print("*** Unknown directory: " + args)
+            else:
+                try:
+                    print(os.getcwd())
+                except:
+                    pass
+        except:
+            pass
+
+    def do_pwd(self, args):
+        """Print current working directory."""
+
+        try:
+            print(os.getcwd())
+        except:
+            pass
+
+    def do_ls(self, args):
+        """List files in a directory."""
+
+        try:
+            if len(args) == 0:
+                for files in os.listdir(os.getcwd()):
+                    print(files)
+            else:
+                for files in os.listdir(args):
+                    print(files)
+        except:
+            print("*** Unknown directory: " + args)
+
+    def do_mkdir(self, args):
+        """Create a new folder."""
+
+        try:
+            os.mkdir(args)
+        except:
+            print("*** Unknown directory: " + args)
+
+    def do_rmdir(self, args):
+        """Remove a folder from device."""
+
+        try:
+            os.rmdir(args)
+        except:
+            print("*** Unknown file or folder: " + args)
+
+    def do_eval(self, args):
+        """Evaluate an expression."""
+
+        try:
+            out = eval(args)
+            print(out)
+        except:
+            print("*** Invalid expression: " + args)
+
+    def do_date(self, args):
+        """Print the date."""
+
+        try:
+            now = time.localtime()
+            print(time.strftime("%a %b %d %H:%M:%S %Y", now))
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    name = Prompt.user + "$ "
+    prompt = Prompt()
+    prompt.prompt = name
+    prompt.cmdloop('PyBash 1.0.0. Type ? for a list of commands.\n')
